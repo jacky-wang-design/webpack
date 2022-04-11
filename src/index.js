3d1842650df3f1a521b56e3f28e8ab3fdad674d8
jacky <49807961+wangxiuting@users.noreply.github.com>
2022-04-11 10:49:04 +0800

    second commit
    second commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.scss';
+
+
+const a = 'Hello ITEM'
+console.log(a, 111111111)
+module.exports = a;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path")
 const HtmlPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin') // 源码中以对象形式导出，并且命名必须与plugin同名（矫情）
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
-  entry: {
-    entry1: './entry/entry1.js',
-    entry2: './entry/entry2.js'
-  },
+  entry:
+    // entry1: './entry/entry1.js',
+    // entry2: './entry/entry2.js'
+    './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.[contenthash].js'
@@ -35,25 +36,34 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, './pubplic'),
+      directory: path.join(__dirname, './src'),
     },
     compress: true,
     port: 9000,
   },
   plugins: [
     new HtmlPlugin({
-      template: path.resolve(__dirname, './pubplic/index.html'),
-      inject: "body",
-      filename: 'index.[contenthash].html'
-
+      template: './src/index.html',
+      // template: path.resolve(__dirname, '/src/index.html'),
+      // inject: "body",
+      filename: 'index.[contenthash:8].html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:8].css'
+    })
   ],
   module: {
     rules: [
       {
-        test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        test: /\.(s[ac]|c)ss$/i, //匹配所有的 sass/scss/css 文件
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader, // 添加 loader
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
       }
     ]
   }
